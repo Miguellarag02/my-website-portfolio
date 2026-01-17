9720679da2688b9c42b688423a3a19c1cfb9bb5e
Miguel Angel <miguelangellarag@gmail.com>
2026-01-17 02:13:45 +0100

    Fixed overlay bug in abilities card

--- src/components/AbilitiesCard.jsx
+++ src/components/AbilitiesCard.jsx
@@ -21,24 +21,24 @@ const AbilitiesCard = (closedAbilitiesTab) => {
   return (
     <div className="relative w-full h-full">
       {/* Grid base */}
-      <div className="w-full h-full relative">
+      <div className="w-full h-full relative perspective-1200 preserve-3d">
         {myAbilities.map((card) => (
           <div
             key={card.id}
             onClick={() => setOpenId(card.id)}
-            className={`ability_card_base  ${openId == card.id ? "ability_card_expanded" : `ability_card_${card.id}`}`}
+            className={`ability_card_base ${openId == card.id ? "ability_card_expanded" : `ability_card_${card.id}`}`}
           > 
-            <div className="ability_title text-2xl mt-[2vh] mx-auto">
+            <div className={`ability_title text-2xl mx-auto transition-transform duration-700 ease-in-out ${openId == card.id ? "translate-y-0" : "translate-y-[50%]"}`}>
               <div className="cosmic_effect" style={{ "--color": "#23b1d8" }} data-text={card.title}>
                 <span>{card.title}</span>
               </div>
             </div>
-            <p className={
-              openId == card.id ? 
-              "text-white/60 text-sm mt-2 leading-relaxed whitespace-pre-line font-semibold fade-in-delay-1" 
-              : "hidden"}>{card.desc}
-            </p>
             <div className={openId == card.id ? "w-full h-full" : "hidden"}>
+              <p className={
+                openId == card.id ? 
+                "text-white/60 items-center justify-center 2xl:text-sm md:text-xs text-[10px] mt-2 leading-relaxed whitespace-pre-line font-semibold fade-in-delay-1" 
+                : "hidden"}>{card.desc}
+              </p>
               <div className="absolute top-[1vh] right-[0.5vw] ">
                 <button
                   className="w-10 h-10 text-2xl flex justify-center text-white/60"
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -65,7 +65,7 @@ const About = () => {
               </div>
             </div>
             <div className="flex items-center justify-center
-                sm:text-base text-xs
+                md:text-base sm:text-xs text-[10px]
                 leading-relaxed
                 text-justify
                 xl:row-span-3 row-span-4
@@ -79,10 +79,10 @@ const About = () => {
           {/* My Abilities */}
           <li className={`grid grid-rows-12 card ${cardPositions[cardStates[cardItems.ABILITIES]]}`} onClick={(e) => rotate(e, cardStates[cardItems.ABILITIES] > cardStates[cardItems.HOBBIES], cardItems.ABILITIES)}>
             <p className="card_tag text-gray row-span-2">My Abilities</p>
-            <div className={`row-span-6 z-100 mr-8 ml-8 ${cardContent[cardStates[cardItems.ABILITIES]]}`} >
+            <div className={`md:row-span-6 row-span-10 z-100 md:mr-8 md:ml-8 md:mb-0 mb-6 ${cardContent[cardStates[cardItems.ABILITIES]]}`} >
               <AbilitiesCard closedAbilitiesTab={cardStates[cardItems.ABILITIES] != 1}/>
             </div>
-            <div className="relative row-span-4 flex items-center justify-center mb-4 mr-10 ml-10 border rounded-lg border-x-teal-200">
+            <div className="relative md:row-span-4 md:flex hidden items-center justify-center mb-4 mr-10 ml-10 border rounded-lg border-x-teal-200">
               <p>Items volando</p>
             </div>
           </li>
